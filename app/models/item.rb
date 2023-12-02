f6ac1ad8e0aba9abfd302f0cd1b9d4528cd9efcd
Jason <jasonzhang501@gmail.com>
2023-12-01 20:42:24 -0500

    Items are displaying

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,4 @@
 class Item < ApplicationRecord
-    validates :name, :cost, :category, :stock, :description, presence: true
-  
-    has_one_attached :photo
-  end
-  
-  
+  validates :name, :cost, :category, :stock, :description, presence: true
+  has_one_attached :photo
+end

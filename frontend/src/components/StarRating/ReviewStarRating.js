8560b47dafd17c844fedce8e1a85daae2066ea5f
Jason <jasonzhang501@gmail.com>
2024-02-19 22:35:05 -0500

    Everything Is Functional. Last Step CSS.

---
+++ frontend/src/components/StarRating/ReviewStarRating.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar as solidStar } from '@fortawesome/free-solid-svg-icons';
+import { faStar as regularStar } from '@fortawesome/free-regular-svg-icons';
+import "./ReviewStarRating.css"
+
+const ReviewStarRating = ({ rating, totalRatings }) => {
+  return (
+    <div>{rating}
+      {[1, 2, 3, 4, 5].map((index) => (
+        <FontAwesomeIcon
+          className='reviewstar'
+          key={index}
+          icon={index <= rating ? solidStar : regularStar}
+        />
+      ))}
+      &nbsp;
+      {totalRatings} ratings
+    </div>
+  );
+};
+
+export default ReviewStarRating;

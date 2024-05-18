aca0beb4d365b9fc324a83d183072c8b295c73c9
swejasonzhang <swejasonzhang@gmail.com>
2024-05-17 23:08:43 -0400

    Seeded 5 more items.

--- db/seeds.rb
+++ db/seeds.rb
@@ -81,6 +81,72 @@ require "open-uri"
 
   CorsairKeyboard.photo.attach(io: URI.open("https://amazeon-seeds.s3.amazonaws.com/Keyboard.jpg"), filename: "Keyboard.jpg")
 
+  Moniter = Item.create!({
+    id: 5,
+    name: "HP 27-inch FHD IPS Gaming Monitor with Tilt/Height Adjustment with AMD FreeSync PremiumTechnology (X27, 2021 model)",
+    cost: 275.00,
+    category: "Monitor",
+    stock: 192,
+    description: "The Eyesafe Certified Display meets TÜV low blue light requirements and Eyesafe standards, protecting your eyes without distorting colors. It features adjustable height, pivot, and tilt for comfortable viewing. The packaging is sustainably sourced, using fewer materials and more recycled content. With AMD FreeSync Premium technology, enjoy a high refresh rate, low framerate compensation, and low latency for confident gaming. A 1-millisecond response time with Overdrive ensures fluid, crisp visuals without motion blur in fast-paced games."
+  })
+
+  Moniter.photo.attach(io: URI.open("https://amazeon-seeds.s3.amazonaws.com/Moniter.jpg"), filename: "Moniter.jpg")
+
+  Mousepad = Item.create!({
+    id: 6,
+    name: "Corsair MM300 - Anti-Fray Cloth Gaming - High-Performance Mouse Pad Optimized for Gaming Sensors - Designed for Maximum Control - Extended, Multi Color",
+    cost: 29.99,
+    category: "Mousepad",
+    stock: 391,
+    description: "The textile weave surface offers superior control with pixel-precise targeting and low friction tracking. Surrounded stitched edges ensure durability by preventing surface peeling. Optimized for both laser and optical gaming mice, it provides accurate and precise movements. The anti-skid rubber base keeps it securely in place."
+  })
+
+  Mousepad.photo.attach(io: URI.open("https://amazeon-seeds.s3.amazonaws.com/Mousepad.jpg"), filename: "Mousepad.jpg")
+
+  VirtualReality = Item.create!({
+    id: 7,
+    name: "Meta Quest 2 — Advanced All-In-One Virtual Reality Headset — 128 GB",
+    cost: 249.99,
+    category: "Virtual Reality",
+    stock: 29,
+    description: "Experience total immersion with 3D audio, hand tracking, and easy-to-use controllers. Explore over 500 titles across various genres, including exclusive VR experiences. Enjoy smooth gameplay and immersive graphics with a fast processor. Socialize in multiplayer arenas, attend live events, and join adventures. The wireless headset features intuitive controls, a built-in battery, and easy setup, with no PC or console needed. Set a play space and get alerts if you move outside it."
+  })
+
+  VirtualReality.photo.attach(io: URI.open("https://amazeon-seeds.s3.amazonaws.com/VirtualReality.jpg"), filename: "VirtualReality.jpg")
+
+  MoniterStand = Item.create!({
+    id: 8,
+    name: "RGB Gaming Computer Monitor Stand Riser with Drawer,Storage and Phone Holder - 1 USB 3.0 and 3 USB 2.0 Hub, 3 Length Adjustable",
+    cost: 32.99,
+    category: "Moniter Stand",
+    stock: 89,
+    description: "The computer stand features 1 USB 3.0 port, 3 USB 2.0 ports, and RGB lights with a memory function, offering 10 lighting effects. Its foldable, adjustable design supports up to 66 lbs and provides ergonomic height options to relieve neck and back fatigue. Suitable for monitors, printers, and laptops, it comes with a 3-year warranty for quality assurance."
+  })
+
+  MoniterStand.photo.attach(io: URI.open("https://amazeon-seeds.s3.amazonaws.com/MoniterStand.jpg"), filename: "MoniterStand.jpg")
+
+  Mic = Item.create!({
+    id: 9,
+    name: "FIFINE XLR/USB Dynamic Microphone for Podcast Recording, PC Computer Gaming Streaming Mic with RGB Light, Mute Button, Headphones Jack, Desktop Stand, Vocal Mic for Singing YouTube-AmpliGame AM8",
+    cost: 64.99,
+    category: "Mic",
+    stock: 53,
+    description: "The podcasting XLR microphone delivers natural audio clarity with a frequency response of 50Hz-16KHz, rejecting unwanted noise for clear vocal recordings. It offers both XLR and USB connections, with features like tap-to-mute, RGB lighting, and a headphone jack for real-time monitoring. Suitable for gaming, streaming, and recording, it includes a noise-canceling windscreen and fits most mic stands."
+  })
+
+  Mic.photo.attach(io: URI.open("https://amazeon-seeds.s3.amazonaws.com/Mic.jpg"), filename: "Mic.jpg")
+
+  PowerStrip = Item.create!({
+    id: 10,
+    name: "Power Strip, SUPERDANNY Surge Protector with 22 AC Outlets and 6 USB Charging Ports, 1875W/15A, 2100 Joules, 6.5Ft Flat Plug Heavy Duty Extension Cord for Home, Office, Dorm, Gaming Room, Black",
+    cost: 69.99,
+    category: "Power Strip",
+    stock: 189,
+    description: "The SUPERDANNY 28-in-1 versatile power strip, certified by FCC, RoHS, and UL, features 22 grounded outlets and 6 USB ports, accommodating all your devices in one location. Its widely spaced outlets and smart USB fast charging, including USB-C, prevent clutter and ensure efficient power delivery. With a mountable flat plug, heavy-duty extension cord, and an extensive 1,000-day warranty, this power strip offers reliable, protected power for your setup."
+  })
+
+  PowerStrip.photo.attach(io: URI.open("https://amazeon-seeds.s3.amazonaws.com/PowerStrip.jpg"), filename: "PowerStrip.jpg")
+
   puts "Done!"
 # end
 
